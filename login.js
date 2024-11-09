7bb9b6ad877bf5faae86dd0a7daf113ca6d72a84
Sarim-Siraj <sarimsirajss@gmail.com>
2024-11-09 14:52:21 +0500

    update

---
+++ login.js
@@ -0,0 +1,13 @@
+function login() {
+    var email = document.getElementById("emaillogin")
+    var passWord = document.getElementById("passwordlogin")
+
+
+    if (email === "sarim123@gmail.com" && passWord === "252895") {
+        alert("Succesfully Login")
+
+    }
+    else {
+        alert("Worng Password")
+    }
+}
